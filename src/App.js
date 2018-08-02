9e3450a28ea9226a077e10df6381189f5519b52c
Adam-Paterson1 <adam.paterson03@gmail.com>
2018-08-02 14:42:33 +1000

    added sockets for realtime updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {subscribeToTimer } from './Socket.js';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    subscribeToTimer((err, timestamp) => this.setState({ 
+      timestamp 
+    }));
+  }
+  state = {
+    timestamp: 'no timestamp yet'
+  };
   render() {
     return (
       <div className="App">
@@ -11,6 +21,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          This is the timer value: {this.state.timestamp}
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
